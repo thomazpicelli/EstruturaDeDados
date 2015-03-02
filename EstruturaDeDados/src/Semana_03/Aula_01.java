d290e55f95d4d590d8b84448b0a9dcab01a6a6cd
thomazpicelli <thomazpicelli@gmail.com>
2015-03-02 08:59:52 -0300

    Quick Sort Aula

---
+++ EstruturaDeDados/src/Semana_03/Aula_01.java
@@ -0,0 +1,79 @@
+package Semana_03;
+
+public class Aula_01 {
+    static long cont = 0;
+    
+    public static void main(String args[]){
+        int[] vetor = new int[500];
+        cont = 0;
+        preencheVetor(vetor);
+        exibe(vetor);
+        quickSort(vetor, 0, vetor.length-1);
+        System.out.println(" ");
+        System.out.println("Total de comparações: " + cont);
+        exibe(vetor);
+    }
+    
+    public static void preencheVetor(int[] vet){        
+    /*    
+        //CASO MEDIO
+        for (int i = 0; i < vet.length; i++) {
+            vet[i] = (int) (5000*Math.random());
+        }
+        
+        //PIOR CASO
+        for (int i = vet.length; i >0; i--) {
+            vet[vet.length-i] = i;
+        }
+        */
+        //MELHOR CASO
+        for (int i = 0; i < vet.length; i++) {
+            vet[i] = i;  
+        }
+        
+    }
+    
+    public static int separa(int[] vetor, int ini, int fim) {
+        int pivo,esq=0;
+        pivo = vetor[ini];
+        while (ini < fim){
+            if (esq==0){
+                cont++;
+                if (pivo >= vetor[fim]){
+                    vetor[ini]=vetor[fim];
+                    ini++;
+                    esq=1;
+                }
+                else
+                    fim--;
+            }
+            else{
+                cont++;
+                if (pivo < vetor[ini]){
+                    vetor[fim]=vetor[ini];
+                    fim--;
+                    esq=0;
+                }
+                else
+                    ini++;
+            }
+        }
+        vetor[fim]=pivo;
+        return fim;
+    }
+    
+    public static void quickSort (int[] vetor, int ini, int fim){
+        int k;
+        if (fim > ini) {
+            k=separa(vetor, ini,fim);
+            quickSort(vetor, ini, k-1);
+            quickSort(vetor, k+1, fim);
+        }
+    }
+
+    public static void exibe(int[] vet){
+        for (int i = 0; i < vet.length; i++) {
+            System.out.print(" " + vet[i]);           
+        }
+    }
+}
